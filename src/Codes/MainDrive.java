b1d39cd7a1bb35479dcd768a983f468f5d3d04da
koolunkle <gugi329@daum.net>
2021-11-15 23:20:30 +0900

    MainDrive 클래스 작성 -> Overloading 연습

--- src/Codes/MainDrive.java
+++ src/Codes/MainDrive.java
@@ -4,6 +4,14 @@ public class MainDrive {
 
 	public static void main(String[] args) {
 
+		MyTest mt = new MyTest();
+
+		mt.myMethod();
+		mt.myMethod(20);
+		mt.myMethod("안녕하세요.");
+		mt.myMethod(20, "안녕하세요");
+		mt.myMethod("안녕하세요", 20);
+
 	}
 
 }
